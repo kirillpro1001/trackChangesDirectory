09d20ef497aa38fac91ef98d077f2f1e3ffe357f
Kirill Bystrov <kir9aza@gmail.com>
2022-11-28 21:57:31 +0300

    create form track directory ftp-server

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Проверка обновления каталога на FTP-сервере</title>
+</head>
+<body>
+	<form action="trackFtp.php" method="post">
+		<label for="ftpServer">
+			Адрес FTP-сервера: <input type="text" name="ftpServer"> 
+		</label>
+		<label for="login">
+			Логин FTP-сервера: <input type="text" name = "login">
+		</label>
+		<label for="password">
+			Пароль FTP-сервера: <input type="password" name = "password">
+		</label>
+		<label for = "ftpDirectory">
+			Директория FTP-сервера: <input type="text" name = "ftpDirectory">
+		</label>
+	</form>
+</body>
+</html>
